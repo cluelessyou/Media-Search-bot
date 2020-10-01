ba85c934caea0899c8d600c66e5737c3bbac3464
cluelessyou <70807249+cluelessyou@users.noreply.github.com>
2020-10-01 20:49:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 async def start(bot, message):
     """Start command handler"""
     buttons = [[
-        InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
+        InlineKeyboardButton('Search Presets', switch_inline_query_current_chat=''),
         InlineKeyboardButton('Go Inline', switch_inline_query=''),
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
